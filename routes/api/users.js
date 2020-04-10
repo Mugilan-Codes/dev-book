083731dace13e527d036fada5da564d980e02472
Mugilan <mugilancodes@gmail.com>
2020-04-10 21:49:45 +0530

    User Model Validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
 
-// @route   GET api/users
-// @desc    Test Route
+// @route   POST api/users
+// @desc    Register User
 // @access  Public
-router.get('/', (req, res) => res.send('Users Route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with minimum of 6 characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    console.log(req.body);
+    res.send('Users Route');
+  }
+);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect to Database
 connectDB();
 
+// Initialize Middleware
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => {
   res.send('API Running...');
 });
