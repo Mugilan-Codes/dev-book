45498d4b235daeda353133c761d2e083f17b7dd6
Mugilan <mugilancodes@gmail.com>
2020-04-10 21:09:18 +0530

    Modified Comments

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-// @desc Connection to MongoDB
+// Connection to MongoDB
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
--- server.js
+++ server.js
@@ -16,6 +16,6 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-// @desc SERVER
+// SERVER
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`));
