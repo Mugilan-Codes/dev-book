9181c736fb4bde51cb447b0d835265ca039245e6
Mugilan <mugilancodes@gmail.com>
2020-04-10 18:08:27 +0530

    Connect to MongoDB

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+// @desc Connection to MongoDB
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
+
+// Connect to Database
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('API Running...');
