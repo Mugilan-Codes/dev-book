317810aff60583db34e4237e3931b5531379c02b
Mugilan <mugilancodes@gmail.com>
2020-04-15 21:02:09 +0530

    Create Profile Component

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-form/CreateProfile';
+
 import PrivateRoute from './components/routing/PrivateRoute';
 
 // Redux
@@ -38,6 +40,11 @@ const App = () => {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute
+                exact
+                path='/create-profile'
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
