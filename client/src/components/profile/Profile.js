22e049dceb49d41eb00a43fa12db83269a99f550
Mugilan <mugilancodes@gmail.com>
2020-04-17 22:46:41 +0530

    Starting Profile

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
@@ -15,7 +16,26 @@ const Profile = ({
     getProfileById(match.params.id);
   }, [getProfileById]);
 
-  return <div>profile</div>;
+  return (
+    <Fragment>
+      {profile === null || loading ? (
+        <Spinner />
+      ) : (
+        <Fragment>
+          <Link to='/profiles' className='btn btn-light'>
+            Back to Profiles
+          </Link>
+          {auth.isAuthenticated &&
+            auth.loading === false &&
+            auth.user._id === profile.user._id && (
+              <Link to='/edit-profile' className='btn btn-dark'>
+                Edit Profile
+              </Link>
+            )}
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 Profile.propTypes = {
